4cf5d5d4b7fc2ec20e35d270da42687e631441c9
Georges Dubus <georges.dubus@compiletoi.net>
2015-01-15 16:05:54 +0100

    Mode the template to its own file

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 
 import Web.Scotty
 
@@ -10,7 +10,7 @@ import Data.List (find)
 import Data.Monoid (mconcat)
 import Data.Text.Lazy (pack)
 import System.Environment (getEnvironment)
-import Text.Hamlet (shamlet)
+import Text.Hamlet (shamletFile)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 
 import Channels (DiffChannel (..), channels, age, jobset)
@@ -21,30 +21,4 @@ main = do
   scotty port $ do
     get "/" $ do
       allChannels <- liftIO $ channels
-      html $ renderHtml [shamlet|
-$doctype 5
-<head>
-  <title> "How outdated are NixOS channels?"
-  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-<body>
-  <div class=container>
-    <div class=page-header>
-      <h1>How outdated are NixOS channels?
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>Channel
-            <th>Updated
-            <th>Tests
-        <tbody>
-          $forall chan <- allChannels
-            <tr>
-              <td>
-                <a href="/#{dname chan}"> #{dname chan} 
-              <td>
-                #{dtime chan} ago
-              <td>
-                <a href="http://hydra.nixos.org/job/#{jobset chan}/tested#tabs-constituents"> Hydra
-  <div class=container>
-    <p class=text-muted>Made by <a href="https://twitter.com/georgesdubus">Georges Dubus</a>, code is on <a href="https://github.com/madjar/howoldis">github</a>
-|]
+      html $ renderHtml $(shamletFile "index.hamlet")
