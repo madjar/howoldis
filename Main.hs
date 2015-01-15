e1b4927ed47ba9c6e4d896f5e1a2fb9a16e8ae8d
Georges Dubus <georges.dubus@compiletoi.net>
2015-01-15 16:34:11 +0100

    The big glowing "nixos-unstable" is back

--- Main.hs
+++ Main.hs
@@ -15,10 +15,21 @@ import Text.Blaze.Html.Renderer.Text (renderHtml)
 
 import Channels (DiffChannel (..), channels, age, jobset)
 
+
 main = do
   env <- getEnvironment
   let port = maybe 3000 read $ lookup "PORT" env
   scotty port $ do
-    get "/" $ do
+    get "/:channel" $ do
       allChannels <- liftIO $ channels
+      channelName <- param "channel"
+      let mainChannel = findChannel channelName allChannels
       html $ renderHtml $(shamletFile "index.hamlet")
+
+
+findChannel :: String -> [DiffChannel] -> DiffChannel
+findChannel channelName chans = fromJust $
+                                lookup channelName
+                                <|> lookup "nixos-unstable"
+                                <|> Just (head chans)
+  where lookup n = find (\c -> dname c == n) chans
